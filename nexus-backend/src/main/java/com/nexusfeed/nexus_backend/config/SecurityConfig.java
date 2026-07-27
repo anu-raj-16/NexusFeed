0f786d3d57c3e3eedcf7140a3a02543ac9c6e0de
anu-raj-16 <anaghag.1612@gmail.com>
2026-07-27 16:27:15 -0600

    fixinf cors error

--- nexus-backend/src/main/java/com/nexusfeed/nexus_backend/config/SecurityConfig.java
+++ nexus-backend/src/main/java/com/nexusfeed/nexus_backend/config/SecurityConfig.java
@@ -27,7 +27,7 @@ public class SecurityConfig {
         http
         .cors(cors -> cors.configurationSource(request -> {
             var config = new org.springframework.web.cors.CorsConfiguration();
-            config.setAllowedOrigins(List.of("*"));
+            config.addAllowedOriginPattern("*");
             config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
             config.setAllowedHeaders(List.of("*"));
             return config;
