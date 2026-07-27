09f24d37c364652fe7196e3e75b538c0f9996966
anu-raj-16 <anaghag.1612@gmail.com>
2026-07-27 16:48:01 -0600

    fixing bugs

--- nexus-backend/src/main/java/com/nexusfeed/nexus_backend/config/SecurityConfig.java
+++ nexus-backend/src/main/java/com/nexusfeed/nexus_backend/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -41,6 +42,7 @@ public class SecurityConfig {
         .cors(cors -> cors.configurationSource(corsConfigurationSource()))
         .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
+                .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 .requestMatchers("/api/auth/**").permitAll()
                 .requestMatchers("/api/jobs/**").permitAll()
                 .requestMatchers("/api/resumes/**").permitAll()
